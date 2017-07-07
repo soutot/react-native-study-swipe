3cbd8ab80328ac9a3341da1fcf243a2f06862344
Tiago Souto <soutot@users.noreply.github.com>
2017-07-06 22:07:33 -0300

    Moving a ball

---
+++ src/Ball.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+
+class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0,0);
+    Animated.spring(this.position, {
+      toValue: {x: 200, y: 500 }
+    }).start()
+  }
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View ref='ball' style={styles.ball} />
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  ball: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    borderWidth: 30,
+    borderColor: '#000000',
+  }
+});
+
+export default Ball;
