1ec8d44588322937b3843022550638d94d56ec79
Tiago Souto <soutot@users.noreply.github.com>
2017-07-07 21:21:36 -0300

    Drag component working

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { 
+  View, 
+  Animated,
+  PanResponder,
+} from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+    
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy })
+      },
+      onPanResponderRelease: () => {},
+    });
+
+    this.state = {
+      panResponder,
+      position,
+    };
+  }
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item); //renderCard(item) is defined in main.js
@@ -9,9 +30,12 @@ class Deck extends Component {
   }
   render() {
     return(
-      <View>
+      <Animated.View 
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
