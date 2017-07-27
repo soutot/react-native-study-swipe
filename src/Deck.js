302252bdae81a692275a33515185e9ac9c18e055
Tiago Souto <soutot@users.noreply.github.com>
2017-07-27 00:17:56 -0300

    Added defaultProps in Deck component

--- src/Deck.js
+++ src/Deck.js
@@ -11,6 +11,10 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {},
+  }
   constructor(props) {
     super(props);
     
@@ -35,6 +39,7 @@ class Deck extends Component {
     this.state = {
       panResponder,
       position,
+      index: 0,
     };
   }
 
@@ -44,7 +49,14 @@ class Deck extends Component {
     Animated.timing(this.state.position, {
       toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION,
-    }).start();
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
   }
 
   resetPosition() {
